868e5ff086d402bbae8bf044bcbf7674d9a8bde3
Elyas SekhavatiNia <e.b35927@gmail.com>
2024-05-23 16:11:01 +0330

    get todo list from api method

--- lib/controller/api/todo_controller.dart
+++ lib/controller/api/todo_controller.dart
@@ -1,3 +1,20 @@
-import 'package:get/get.dart';
+import 'dart:convert';
 
-class TodoController extends GetxController {}
+import 'package:get/get.dart';
+import 'package:todo_list/models/todo_model.dart';
+import 'package:http/http.dart' as http;
+
+class TodoController extends GetxController {
+  var todoList = RxList<Todo>();
+  var url =
+      Uri.parse('https://664f2b28fafad45dfae2a3cd.mockapi.io/api/todolist');
+  Future<RxList<Todo>> getTodoList() async {
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      for (var item in jsonDecode(response.body)) {
+        todoList.add(Todo.fromJson(item));
+      }
+    }
+    return todoList;
+  }
+}
