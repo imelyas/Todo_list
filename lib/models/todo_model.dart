2030585d05914bc6cb0f2c9f6ae8d50a328a6fab
Elyas SekhavatiNia <e.b35927@gmail.com>
2024-05-23 19:18:44 +0330

    add status and created_at fields

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -2,15 +2,21 @@ class Todo {
   Todo({
     required this.title,
     required this.desc,
+    required this.status,
+    required this.createdAt,
     required this.id,
   });
   late final String title;
   late final String desc;
+  late final bool status;
+  late final String createdAt;
   late final String id;
-
-  Todo.fromJson(Map<String, dynamic> json) {
+  
+  Todo.fromJson(Map<String, dynamic> json){
     title = json['title'];
     desc = json['desc'];
+    status = json['status'];
+    createdAt = json['created_at'];
     id = json['id'];
   }
 
@@ -18,6 +24,8 @@ class Todo {
     final data = <String, dynamic>{};
     data['title'] = title;
     data['desc'] = desc;
+    data['status'] = status;
+    data['created_at'] = createdAt;
     data['id'] = id;
     return data;
   }
